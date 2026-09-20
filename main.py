4238f5845badc3391adeb4546c10025069e6a8a7
borregogstec712 <a01715889@tec.mx>
2026-09-19 20:27:18 -0600

    A3

--- main.py
+++ main.py
@@ -3,39 +3,50 @@
     #-> Osea que guarde en unps archivos los estados pasados de los archivos???
     #-> Mejor que genere un archivo final y conserve los originales
     #-> Que solo genere un nuevo archivo, que le ponga el nombre en comun y que los pasados los guarde con nombres diferentes basandose en si eran la carpeta 1 o 2
-
+    #-> Inicialmente habia considerado hacer una comparación de archivos directa basandome en el sort que obtenia al ordenas basandome en comparación directa, pero le añade demasiado tiempo, por lo que decidi modificar el algoritmo y mejor hacer una comparación en arbol basandome en las primeras letras y peso, osea, solo se compara el archivo contra el subset de archivos con la misma letra, que este como en arbol, y luego se hace la comparación por nombre y peso, pero no se hace directamente en la lista original
 
 ##-- CODIGO --##
 
 # Importar bibliotecas para manipular archivos
-
+from pathlib import Path
 
 # FUNCIONES
-
-## validarCarpeta(c) -> Verifica que las carpetas existan en la ruta correcta
-def validarCarpetas():
-
-
-def obtenerArchivos(c):
-    #-> Obtiene los archivos de una carpeta
-    #-> Crea un diccionario    
-    #-> Ejecuta un ciclo for basandose en el 
-    #-> Sort el archivo
-    #-> Return el diccionario
 
 def compararNombres(a, b):
     #-> Shortcut para escribir menos y solo escribor a[0] y b[0] para comparar
     #-> Return Bool
+    return bool
 
 def compararPeso(a, b):
     #-> Shortcut para escribir menos y solo escribor a[0] y b[0] para comparar
     #-> Return Bool
+    return bool
 
 def tamañoCarpeta(c):
+    ruta_carpeta = Path("c")
+    n = sum(1 for archivo in ruta_carpeta.iterdir() if archivo.is_file())
+    return n
     #-> Regresa el numero de archivos en una carpeta
 
+
+## validarCarpeta(c) -> Verifica que las carpetas existan en la ruta correcta, y que las dos esten en la misma carpeta
+def validarCarpetas():
+
+    return bool
+
+
+def obtenerArchivos(c):     # Obtiene los archivos de una carpeta y su peso
+    f = {}          #-> Crea un diccionario  -> Actualización: Mejor usar un diccionario de diccionario  
+    #-> Ejecuta un ciclo for basandose en el tamaño de la carpeta
+    #-> Sort los archivos por orden alfabetico
+    return f #-> Return el diccionario de diccionarios
+
+
+
 def compararCarpetas(a, b):
-    f = []    # Crea un diccionario final
+    # Hace un diccionario final con los nombres de los archivos y al hacer la comparación de una vez agrega el archivo final a la carpeta
+
+    f = {}    # Crea un diccionario final
     size_a , size_b = tamañoCarpeta(a) , tamañoCarpeta(b)
     size = 0
     if size_a > size_b :
@@ -46,8 +57,12 @@ def compararCarpetas(a, b):
 
      
     for i in range(size):
-        
-
+        print(i)
+        # Hacer comparación de nombre primero
+            # Si el nombre no se repite se agrega a f directamente
+                # Si el nombre si se repite se compara por peso del archivo
+                    # A futuro, como solo lo voy a usar con archivos md, se puede hacer una función que evalue tambien linea por linea, si se desea 100% de seguridad, claro es demasiado imrpobable y aumenta demasiado el tiempo, por lo que se podria implementar solo con archivos especificos mencionados en las preguntas iniciales
+            # Al finalizar la comparación se agregan el o los archivos a la carpeta final
 
 
     return f
@@ -63,8 +78,8 @@ def estatusCarpetas(a, b):
 def main():
     print("Hola Bienvenido a el comparador de archivos")
     n = int(input("Cuantos archivos deseas comparar? "))
-    ruta1 = str(input("carpeta1: "))
-    ruta2 = str(input("carpeta2: "))
+    ruta1 = str(input("carpeta1: "))        # Pide la ruta 1
+    ruta2 = str(input("carpeta2: "))        # Pide la ruta 2
 
 
 
