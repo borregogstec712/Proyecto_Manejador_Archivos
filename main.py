0149cb3c39bd46db3b340a56b4371e79fe61e104
borregogstec712 <a01715889@tec.mx>
2026-09-19 19:40:25 -0600

    A2

---
+++ main.py
@@ -0,0 +1,76 @@
+##-- NOTAS e IDEAS --##
+# Considerar hacer control de versiones muy basico?
+    #-> Osea que guarde en unps archivos los estados pasados de los archivos???
+    #-> Mejor que genere un archivo final y conserve los originales
+    #-> Que solo genere un nuevo archivo, que le ponga el nombre en comun y que los pasados los guarde con nombres diferentes basandose en si eran la carpeta 1 o 2
+
+
+##-- CODIGO --##
+
+# Importar bibliotecas para manipular archivos
+
+
+# FUNCIONES
+
+## validarCarpeta(c) -> Verifica que las carpetas existan en la ruta correcta
+def validarCarpetas():
+
+
+def obtenerArchivos(c):
+    #-> Obtiene los archivos de una carpeta
+    #-> Crea un diccionario    
+    #-> Ejecuta un ciclo for basandose en el 
+    #-> Sort el archivo
+    #-> Return el diccionario
+
+def compararNombres(a, b):
+    #-> Shortcut para escribir menos y solo escribor a[0] y b[0] para comparar
+    #-> Return Bool
+
+def compararPeso(a, b):
+    #-> Shortcut para escribir menos y solo escribor a[0] y b[0] para comparar
+    #-> Return Bool
+
+def tamañoCarpeta(c):
+    #-> Regresa el numero de archivos en una carpeta
+
+def compararCarpetas(a, b):
+    f = []    # Crea un diccionario final
+    size_a , size_b = tamañoCarpeta(a) , tamañoCarpeta(b)
+    size = 0
+    if size_a > size_b :
+        size = a
+    else:
+        size = b
+
+
+     
+    for i in range(size):
+        
+
+
+
+    return f
+
+def estatusCarpetas(a, b):
+    eliminados = 0
+    copiados = 0
+    creados = 0
+
+ 
+# MAIN
+
+def main():
+    print("Hola Bienvenido a el comparador de archivos")
+    n = int(input("Cuantos archivos deseas comparar? "))
+    ruta1 = str(input("carpeta1: "))
+    ruta2 = str(input("carpeta2: "))
+
+
+
+    
+
+    
+
+
+main()
